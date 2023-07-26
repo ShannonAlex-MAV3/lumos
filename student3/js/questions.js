9e4c573516e12cdc116afdb5e17fcae1be2e0d92
Shannon Akmeemana <100816252+ShannonAlex-MAV3@users.noreply.github.com>
2023-07-26 21:47:46 +0530

    Add files via upload
    student three upload

---
+++ student3/js/questions.js
@@ -0,0 +1,83 @@
+
+let questions = [
+    {
+    numb: 1,
+    question:"What is the main purpose of using Artificial Intelligence (AI) in computer systems?",
+    answer: "To make computers smarter and capable of human-like tasks",
+    options: [
+      "To make computers faste",
+      "To make computers more reliable",
+      "To make computers smarter and capable of human-like tasks",
+      "To reduce the cost of computer hardware"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which AI application uses algorithms to analyze large datasets and discover patterns and insights?",
+   
+    answer:"Data Mining",
+    options: [
+      "Natural Language Processing",
+      "Computer Vision",
+      "Data Mining",
+      "Speech Recognition"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which subfield of AI is focused on enabling computers to understand and process human language?",
+    answer: "Natural Language Processing",
+    options: [
+      "Robotics",
+      "Neural Networks",
+      "Natural Language Processing",
+      "Expert Systems"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the primary purpose of using Recurrent Neural Networks (RNNs) in AI applications? ",
+    answer:"Natural language understanding",
+    options: [
+      "Image recognition",
+      "Speech synthesis",
+      "Natural language understanding",
+      "Real-time data processing"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which type of AI learns through trial and error and receives feedback in the form of rewards or penalties?",
+    answer: "Reinforcement Learning",
+    options: [
+      "Unsupervised Learning",
+      "Deep Learning",
+      "Reinforcement Learning",
+      "Natural Language Processing"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "What is the function of a chatbot powered by AI",
+    answer: "Simulating conversations with real users",
+    options: [
+      "Identifying emotions of users",
+      "Simulating conversations with real users",
+      "Making phone calls on behalf of users",
+      "Analyzing user behaviors on social media"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does NLP stand for in the context of AI?",
+    answer: "Natural Language Processing",
+    options: [
+      "Natural Language Processing",
+      "Neural Language Programming",
+      "Natural Linguistic Processor",
+      "Non-linear Linguistic Protocol"
+    ]
+  },
+  
+];
